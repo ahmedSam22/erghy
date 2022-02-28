f0b05879def74c4cd8fc203d20f4ccf453d14aa8
ahmedSam22 <58396761+ahmedSam22@users.noreply.github.com>
2022-02-28 22:12:08 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route,Link } from "react-router-dom";
+import Home from './pages/Home';
+import Chat from './pages/Chat';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Route path="/home" exact component={Home} />
+          <Route path="/chat" exact component={Chat} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Home() {
+
+    
+  return (
+    <div className="App">
+      <Link to="/">backkkkk</Link>
+    </div>
+  );
+}
+
+export default Home;
