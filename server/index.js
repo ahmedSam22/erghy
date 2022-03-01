68b57555e0128c647f4ade020564b7a316395e4a
ahmedSam22 <58396761+ahmedSam22@users.noreply.github.com>
2022-03-01 02:53:19 +0200

    str

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const chats = require("./data/data");
+const app = express();
+
+
+
+app.get('/',(req,res)=>{ 
+    res.send("hhh");
+})
+
+app.get('/api/chat',(req,res)=>{
+    res.send(chats);
+})
+
+app.get('/api/chat/:id',(req,res)=>{
+    const id = req.params.id;
+    const singleChat = chats.find((e)=>e._id === id)
+    res.send(singleChat)
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , ()=>{
+    console.log("tamam");
+})
